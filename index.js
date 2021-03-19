3e3cff3cbd0320bedc431006a561288b518734f9
lucasadlerstein <lucasadlerstein@gmail.com>
2021-03-19 12:36:07 -0300

    443 and PM2

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Puerto de la app
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 443;
 
 // Rutas
 app.use('/api/cocteles', require('./routes/cocteles'));
